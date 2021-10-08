a0d7798396c90f68042dda67053a2264fcf6f247
adam_kinyua <datascience.stud.2021@gmail.com>
2021-10-08 18:18:33 -0400

    Still learning

---
+++ vendetta_0.f90
@@ -0,0 +1,20 @@
+ program vendetta_0
+
+   implicit none
+
+   write(*,*)'FIRST CODE BLOCK'
+
+   print*, 'format list'
+   print*  !this line adds a blank line 
+   print*, 'format list'
+   write(*,*) !this one too adds a blank line 
+   write(*,*) 'format list'
+   print*
+
+   write(*,*) "SECOND CODE BLOCK"
+  
+   print "A", 'simple text'
+  
+
+ end program vendetta_0
+
